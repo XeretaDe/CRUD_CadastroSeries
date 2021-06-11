b5d1a225b3c012f37b32638d0c3a502042d6db7f
XeretaDe <rodrigomcariello@gmail.com>
2021-06-11 15:02:08 -0300

    Add_Console.dotnet
    Add de console com o comando dotnet new console.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_CadastroSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
