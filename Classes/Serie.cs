30f7a11b7ec2f36d32743a554f0e667a7708fdfc
XeretaDe <rodrigomcariello@gmail.com>
2021-06-11 19:38:18 -0300

    ADD_Reps+Cadastros+ProjectFront
    Add dos repositórios de informações, integrações por meio de voids externos (aplicando bons métodos para manutenção de código), Add do versionamento de cadastros, e adição do "FrontEnd" do console para que o usuário possa navegar.

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -10,6 +10,8 @@ namespace CRUD_CadastroSeries.Classes
         private string Descricao {get;set;}
 
         private int Ano{get;set;}
+        private bool Excluido{get;set;}
+
 
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
@@ -18,6 +20,8 @@ namespace CRUD_CadastroSeries.Classes
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
+
         }
 
         public override string ToString()
@@ -27,7 +31,23 @@ namespace CRUD_CadastroSeries.Classes
             returning += "Título: " + this.Titulo + Environment.NewLine;
             returning += "Descrição: " + this.Descricao + Environment.NewLine;
             returning += "Ano de ínicio: " + this.Ano + Environment.NewLine;
+            returning += "Excluido: " + this.Excluido;
             return returning; 
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public string retornaId()
+        {
+            return this.Id.ToString();
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+
         }
 
     }
